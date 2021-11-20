555d5d0671c452c8e5eb17efc168d26398ed2c34
Jakub Landowski <landowski222@gmail.com>
2021-11-20 12:54:16 +0100

    Gear and lights commit

--- objectProgramming/src/Car.java
+++ objectProgramming/src/Car.java
@@ -1,6 +1,9 @@
 public class Car {
 
     private int speed = 0;
+    private int gear;
+    private String name;
+    private boolean lights = false;
 
     public void startEngine(){
         System.out.println("Silnik działa");
@@ -22,5 +25,31 @@ public class Car {
             System.out.println("Samochód zatrzymał się");
         }
     }
+
+    public void setGear(int gear){
+        if (gear > 0 && gear < 7){
+            this.gear = gear;
+            System.out.println("Aktywny bieg "+this.gear);
+        }
+        else System.out.println("Nie ma takiego biegu");
+    }
+    public int getGear(){
+        return this.gear;
+    }
+
+    public void switchLights(){
+        lights = !lights;
+        if (lights == true)
+            System.out.println("światła włączone");
+        else System.out.println("światła wyłączone");
+    }
+    public void setLights(boolean lights){
+        this.lights = lights;
+    }
+    public boolean getLights(){
+        return lights;
+    }
+
+
 
 }
--- objectProgramming/src/Main.java
+++ objectProgramming/src/Main.java
@@ -3,10 +3,17 @@ public class Main {
 
         Car audi = new Car();
         audi.startEngine();
+        audi.setLights(true);
+        System.out.println("Sprawdzam światła ... "+audi.getLights());
+        audi.setGear(1);
         audi.accelerate(20);
+        audi.setGear(2);
         audi.accelerate(20);
         audi.decelerate(15);
         audi.decelerate(66);
+        System.out.println("Sprawdzam bieg ... "+audi.getGear());
         audi.stopEngine();
+        audi.switchLights();
+
     }
 }
